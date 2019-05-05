cfece32c7bf54a0ff08eb45ec6e13122d5f2109e
Maxxx911 <maxrikal@gmail.com>
2019-05-05 23:39:56 +0300

    Add basic class and schema to design

---
+++ Kurs_work_6sem/Kurs_work_6sem/NetworkStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs_work_6sem
+{
+    class NetworkStatus
+    {
+        // Получение имени компьютера.
+        static String host = System.Net.Dns.GetHostName();
+
+        // Получение ip-адреса.
+        readonly System.Net.IPAddress ip = System.Net.Dns.GetHostByName(host).AddressList[0];
+        // Показ адреса в label'е.
+       
+    }
+}
---
+++ Kurs_work_6sem/Kurs_work_6sem/PCStatus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs_work_6sem
+{
+    static class PCStatus
+    {
+        public static void ShowSystemInfo()
+        {
+            Console.WriteLine("Операционная система (номер версии):  {0}", Environment.OSVersion);
+            Console.WriteLine("Разрядность процессора:  {0}", Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE"));
+            Console.WriteLine("Модель процессора:  {0}", Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER"));
+            Console.WriteLine("Путь к системному каталогу:  {0}", Environment.SystemDirectory);
+            Console.WriteLine("Число процессоров:  {0}", Environment.ProcessorCount);
+            Console.WriteLine("Имя пользователя: {0}", Environment.UserName);
+            // Локальные диски
+            Console.WriteLine("Локальные диски: ");
+            foreach (DriveInfo dI in DriveInfo.GetDrives())
+            {
+                Console.Write(
+                      "\t Диск: {0}\n\t" +
+                      " Формат диска: {1}\n\t " +
+                      "Размер диска (ГБ): {2}\n\t Доступное свободное место (ГБ): {3}\n",
+                      dI.Name, dI.DriveFormat, (double)dI.TotalSize / 1024 / 1024 / 1024, (double)dI.AvailableFreeSpace / 1024 / 1024 / 1024);
+                Console.WriteLine();
+            }
+        }
+    }
+}
